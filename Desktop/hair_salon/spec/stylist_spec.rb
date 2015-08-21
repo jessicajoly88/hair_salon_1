6c1c115808ee7c0aaa4a4a00b7e40d1c72498651
Jessica <jessicajoly88@gmail.com>
2015-08-21 13:46:29 -0700

    Add delete method from Stylist class

--- Desktop/hair_salon/spec/stylist_spec.rb
+++ Desktop/hair_salon/spec/stylist_spec.rb
@@ -48,4 +48,16 @@ describe(Stylist) do
     end
   end
 
+  describe('#delete') do
+  	it('lets you delete stylists in the database') do
+  	  stylist = Stylist.new({:name => 'Anna', :id => nil})
+      stylist.save()
+      stylist2 = Stylist.new({:name => 'Anna', :id => nil})
+      stylist2.save()
+      stylist.delete()
+      expect(Stylist.all()).to(eq([stylist2]))	
+    end
+  end
+
+
 end
